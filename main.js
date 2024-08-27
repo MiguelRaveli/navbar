198df2bbc737cf2122e84f0d89192969b1b270a4
Raveli <miguelraveli123@gmail.com>
2024-08-27 12:07:22 -0300

    update html and css and js

---
+++ main.js
@@ -0,0 +1,8 @@
+const width = document.body.clientWidth;
+const navBar = document.querySelector(".navBar");
+const header = document.querySelector("#header");
+const three_bars = document.querySelector(".three-bars");
+
+three_bars.addEventListener(("click"), () =>{
+    header.classList.add("")
+})
