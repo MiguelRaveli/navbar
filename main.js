8063c0afd2a3190611bf1061f3a2086d27f76fe4
Raveli <miguelraveli123@gmail.com>
2024-08-27 21:27:14 -0300

    finish nabar

--- main.js
+++ main.js
@@ -1,8 +1,44 @@
-const width = document.body.clientWidth;
 const navBar = document.querySelector(".navBar");
 const header = document.querySelector("#header");
 const three_bars = document.querySelector(".three-bars");
+let buttonTriggered = false;
 
-three_bars.addEventListener(("click"), () =>{
-    header.classList.add("")
-})
+window.addEventListener("DOMContentLoaded", () => {
+  const width = window.innerWidth;
+  if (width > 600) {
+    navBar.classList.add("show");
+    header.classList.remove("floatNavBar");
+  } else {
+    navBar.classList.remove("show");
+    header.classList.add("floatNavBar");
+  }
+});
+
+window.addEventListener("resize", function () {
+  const width = window.innerWidth;
+  if (width > 600) {
+    buttonTriggered = false;
+    navBar.classList.add("show");
+    header.classList.remove("floatNavBar");
+    three_bars.style.transition = "transform 0.2s";
+    three_bars.style.transform = "rotate(0)";
+  } else if (width <= 600 && !buttonTriggered) {
+    navBar.classList.remove("show");
+    header.classList.add("floatNavBar");
+  }
+});
+
+three_bars.addEventListener("click", () => {
+  if (navBar.classList.contains("show") && buttonTriggered) {
+    buttonTriggered = false;
+    navBar.classList.remove("show");
+    three_bars.style.transition = "transform 0.2s";
+    three_bars.style.transform = "rotate(0)";
+  } else {
+    buttonTriggered = true;
+    header.classList.add("floatNavBar");
+    navBar.classList.add("show");
+    three_bars.style.transition = "transform 0.2s";
+    three_bars.style.transform = "rotate(-0.25turn)";
+  }
+});
